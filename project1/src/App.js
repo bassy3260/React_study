ca4302f6f92bce1cd12bd4989a7c8ddfba9b19c3
bassy3260 <bassy3260@naver.com>
2023-07-23 23:12:21 +0900

    Chapter6

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,19 +1,32 @@
 import './App.css';
-import {useState} from "react";
+import {useState, useEffect,useRef} from "react";
 import Viewer from './component/Viewer';
+import Even from "./component/Even";
 import Controller from './component/Controller';
 
 function App() {
   const [count,setCount] =useState(0);
+  const [text, setText] =useState("");
+
   const handleSetCount = (value) =>{
     setCount(count+value);
   };
-  
+
+  const handleChangeText = (e) =>{
+    setText(e.target.value);
+  };
+
+  const didMountRef=useRef(false);
+
   return(
   <div className="App"> 
     <h1>Simple Counter</h1>
     <section>
+      <input value={text} onChange={handleChangeText}/>
+    </section>
+    <section>
       <Viewer count={count}/>
+      {count %2 === 0 && <Even />}
     </section>
     <section>
       <Controller handleSetCount={handleSetCount}/>
