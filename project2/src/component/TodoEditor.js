ed14ef5141c4a6208b831b849bd85242d806eaef
bassy3260 <bassy3260@naver.com>
2023-07-25 18:22:59 +0900

    230725

--- project2/src/component/TodoEditor.js
+++ project2/src/component/TodoEditor.js
@@ -1,11 +1,19 @@
 import "./TodoEditor.css"
-
-const TodoEditor = () =>{
+import {useState} from "react";
+const TodoEditor = ({onCreate}) =>{
+    const [content, setContent] =useState("");
+    const onChangeContent = (e)=>{
+        setContent(e.target.value);
+    };
     return(
     <div className ="TodoEditor">
       <h4>새로운 Todo 작성하기 ✏</h4>
         <div className = "editor_wrapper">
-            <input placeholder = "새로운 Todo..."/>
+            <input 
+            value={content}
+            onChange={onChangeContent}
+            placeholder = "새로운 Todo..."
+            />
             <button>추가</button>
         </div>  
     </div>
